9aac6123c8a76c3d3ff8913eaa948d04c7bef407
Ella <ella.code.j@gmail.com>
2023-08-14 16:16:46 +0900

    Feat: USER_ID(사용자 구분자) 찾는 메서드 추가 - findUserIdBySecurity   = SecurityContextHolder로 user 구분자(USER_ID) 값 찾는 메서드 - findUserIdByUserName   = userName(로그인아이디)로 user 구분자(USER_ID) 값 찾는 메서드

--- src/main/java/com/jiwooja/jiwoojaserver/service/UserService.java
+++ src/main/java/com/jiwooja/jiwoojaserver/service/UserService.java
@@ -4,9 +4,11 @@ import com.jiwooja.jiwoojaserver.domain.Authority;
 import com.jiwooja.jiwoojaserver.domain.User;
 import com.jiwooja.jiwoojaserver.dto.UserDto;
 import com.jiwooja.jiwoojaserver.exception.DuplicateUserException;
+import com.jiwooja.jiwoojaserver.exception.NotFoundUserException;
 import com.jiwooja.jiwoojaserver.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -40,6 +42,28 @@ public class UserService {
                 .authorities(Collections.singleton(authority))
                 .build();
         return userRepository.save(user);
+    }
+
+
+    /**
+     * SecurityContextHolder로 user 구분자(USER_ID) 값 찾는 메서드
+     * @return Long : user 구분자(USER_ID)
+     */
+    public Long findUserIdBySecurity(){
+        org.springframework.security.core.userdetails.User userSecurity = (org.springframework.security.core.userdetails.User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User user = userRepository.findByUsername(userSecurity.getUsername())
+                .orElseThrow(() -> new NotFoundUserException("가입되지 않은 유저입니다."));
+        return user.getUserId();
+    }
+
+    /**
+     * 로그인 아이디로 user 구분자(USER_ID) 값 찾는 메서드
+     * @return Long : user 구분자(USER_ID)
+     */
+    public Long findUserIdByUserName(String userName){
+        User user = userRepository.findByUsername(userName)
+                .orElseThrow(() -> new NotFoundUserException("가입되지 않은 유저입니다."));
+        return user.getUserId();
     }
 
 }
